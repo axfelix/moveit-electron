9ba9363d05af198170c7398e3c661eb5a69ff773
Alex Garnett <axfelix@gmail.com>
2019-07-22 13:51:42 -0700

    first commit, documentation coming soon

---
+++ gui/main.js
@@ -0,0 +1,147 @@
+"use strict";
+
+if(require('electron-squirrel-startup')) return;
+require('update-electron-app')();
+const {app, dialog, nativeImage, shell, Tray, Menu, BrowserWindow} = require("electron");
+const notifier = require("node-notifier");
+const zerorpc = require("zerorpc");
+global.client = new zerorpc.Client();
+const portfinder = require("portfinder");
+const fs = require('fs');
+
+const path = require('path')
+const PY_MOVEIT_FOLDER = 'moveit'
+const PY_FOLDER = '..'
+const PY_MODULE = 'moveit'
+
+let pythonChild = null
+let mainWindow = null
+
+var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+  notifier.notify({"title" : "MoveIt", "message" : "MoveIt is already running."});
+});
+
+if (shouldQuit) {
+  app.quit();
+  return;
+}
+
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+const guessPackaged = () => {
+  const fullPath = path.join(__dirname, PY_MOVEIT_FOLDER)
+  return require('fs').existsSync(fullPath)
+}
+
+const getScriptPath = () => {
+  if (!guessPackaged()) {
+    return path.join(__dirname, PY_FOLDER, PY_MODULE + '.py')
+  }
+  radiamAutoLauncher.isEnabled().then(function(isEnabled){
+    if(!isEnabled){
+      radiamAutoLauncher.enable();
+    }
+  })
+  if (process.platform === 'win32') {
+    return path.join(__dirname, PY_MOVEIT_FOLDER, PY_MODULE + '.exe')
+  }
+  return path.join(__dirname, PY_MOVEIT_FOLDER, PY_MODULE)
+}
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 550,
+    height: 700,
+    backgroundColor: "#D6D8DC",
+  });
+
+  if (app.dock) { app.dock.show() };
+
+  mainWindow.setMenuBarVisibility(false);
+
+  mainWindow.loadURL(require('url').format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.on('close', (event) => {
+    if (mainWindow != null){
+      mainWindow.hide();
+    }
+    mainWindow = null
+  });
+}
+
+
+app.on('ready', () => {
+  createWindow();
+})
+
+portfinder.basePort = 4242;
+let port = portfinder.getPort(function (err, port) {
+  client.connect("tcp://127.0.0.1:" + String(port));
+  const createMoveIt = () => {
+    let script = getScriptPath()
+    if (guessPackaged()) {
+      pythonChild = require('child_process').spawn(script, [port])
+    } else {
+      pythonChild = require('child_process').spawn('python3', [script, port])
+    }
+
+    if (pythonChild != null) {
+      console.log('Python started successfully')
+
+      pythonChild.stdout.on('data', function (data) {
+        console.log(data.toString());
+      });
+
+      pythonChild.stderr.on('data', function (data) {
+        console.log(data.toString());
+      });
+    }
+  }
+
+  app.on('ready', createMoveIt);
+});
+
+const exitMoveIt = () => {
+  pythonChild.kill()
+  pythonChild = null
+}
+
+app.on("before-quit", ev => {
+  if (mainWindow != null){
+    mainWindow.close();
+  }
+  top = null;
+});
+
+app.on('will-quit', ev => {
+  exitMoveIt();
+  app.quit();
+})
+
+if (process.argv.slice(-1)[0] === '--run-tests') {
+  sleep(2000).then(() => {
+    const total_tests = 1
+    let tests_passing = 0
+    let failed_tests = []
+
+    if (pythonChild != null) {
+      tests_passing++;
+    } else {
+      failed_tests.push('spawn_python');
+    }
+
+    console.log(`of ${total_tests} tests, ${tests_passing} passing`);
+
+    if (tests_passing < total_tests) {
+      console.error(`failed tests: ${failed_tests}`);  
+    }
+
+    app.quit();
+  });
+};
+
+let top = {};
---
+++ moveit.py
@@ -0,0 +1,66 @@
+"""
+GUI tool to create a Bag from a filesystem folder.
+"""
+
+import sys
+import os
+import shutil
+import bagit
+from time import strftime
+from distutils.dir_util import copy_tree
+import tempfile
+import zerorpc
+
+
+def generate_file_md5(zipname, blocksize=2**20):
+    m = hashlib.md5()
+    with open(zipname, "rb") as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            m.update(buf)
+    return m.hexdigest()
+
+
+class MoveIt(object):
+    def package(self, contactname, department, email, phone, creator, rrsda, title, datefrom, dateto, description, metadata, package_folder):
+        bag_dir_parent = tempfile.mkdtemp()
+        if os.path.isdir(bag_dir_parent):
+            shutil.rmtree(bag_dir_parent)
+        bag_dir = os.path.join(bag_dir_parent, 'bag')
+        os.makedirs(bag_dir)
+        copy_tree(package_folder, bag_dir)
+
+        try:
+            bag = bagit.make_bag(bag_dir, None, 1, ['md5'])
+            bag.info['Transfer Time'] = strftime("%Y-%m-%d %H:%M:%S")
+            bag.info['Contact Name'] = contactname
+            bag.info['Department'] = department
+            bag.info['Email'] = email
+            bag.info['Phone'] = phone
+            bag.info['Record Creator'] = creator
+            bag.info['RRSDA Number'] = rrsda
+            bag.info['Transfer Title'] = title
+            bag.info['Date Range'] = (datefrom + "-" + dateto)
+            bag.info['Descrption'] = description
+            bag.info['Other Metadata'] = metadata
+            bag.save()
+        except (bagit.BagError, Exception) as e:
+            return False
+
+        bag_destination = os.path.join(str(bag_dir_parent), ("bagged"))
+        zipname = shutil.make_archive(bag_destination, 'zip', bag_dir)
+        shutil.rmtree(bag_dir)
+
+        desktopPath = os.path.expanduser("~/Desktop/")
+        outputPath = desktopPath + os.path.splitext(os.path.basename(zipname))[0]
+        os.mkdir(outputPath)
+        shutil.move(zipname, os.path.join(outputPath, os.path.basename(zipname)))
+        os.remove(zipname)
+        return True
+
+if __name__ == '__main__':
+    s = zerorpc.Server(MoveIt())
+    s.bind('tcp://127.0.0.1:' + str(sys.argv[1]))
+    s.run()
